2971b1e4b23617bef41625f4296afada2fa0a1c4
enzonjagi <amosndungo@gmail.com>
2016-07-16 14:50:50 +0300

    printing changes

--- students.py
+++ students.py
@@ -13,8 +13,8 @@ full_name = student.get("name")
 last_name = full_name[1]
 year_of_birth = student.get("date-of-birth")
 age = current_year - year_of_birth
-print (last_name)
-print (full_name[0], full_name[1])
-print (student.get("phone number"))
-print (year_of_birth)
-print (age)
+print last_name
+print full_name[0], full_name[1]
+print student.get("phone number")
+print year_of_birth
+print age
